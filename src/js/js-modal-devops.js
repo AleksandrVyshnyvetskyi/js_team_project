e1fda40b25ecdb02587ee7d6cf8a2fe568b56c4e
Aleksandr Vyshnyvetskyi <vishniveckijj1994@gmail.com>
2022-08-25 21:36:39 +0300

    Start project

---
+++ src/js/js-modal-devops.js
@@ -0,0 +1,27 @@
+
+// ==========================================================================
+const modalOpen = document.querySelector('.modal-open');
+const modalBox = document.querySelector('.modal-dev__box ');
+const btnFooter = document.querySelectorAll('.footer__btn'); 
+const closeBtn= document.querySelector('.modal-close');
+
+btnFooter.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        modalOpen.classList.add('active');
+        modalBox.classList.add('active');
+    })
+});
+
+closeBtn.addEventListener('click',() => {
+    modalOpen.classList.remove('active');
+    modalBox.classList.remove('active');
+});
+
+document.addEventListener('click', (e) => {
+    if(e.target === modalOpen) {
+        modalOpen.classList.remove('active');
+        modalBox.classList.remove('active');
+    }
+});
+// ==========================================================================
