00ce2e599f8e94f89b7b4f684c2dcf2ff3d143ab
Ilja Keldanovich <ilja.keld@gmail.com>
2022-08-29 14:02:08 +0200

    authentication. modal is done, functionality almost done

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,61 +1,132 @@
-// import { initializeApp } from 'firebase/app';
-// import firebase from 'firebase/compat/app';
-// import * as firebaseui from 'firebaseui'
-// import 'firebaseui/dist/firebaseui.css'
-
+import { initializeApp } from 'firebase/app';
+import {
+    getAuth,
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+    AuthErrorCodes,
+    onAuthStateChanged
+} from "firebase/auth";
 
 // Modal for authorisation
 
 const modalOpen = document.querySelector('.modal-open');
-const modalBox = document.querySelector('.modal-dev__box ');
-const btnAuth = document.querySelectorAll('.auth_btn'); 
-const closeBtn = document.querySelector('.modal-close');
+const modalBox = document.querySelector('.modal-authent__box');
+const btnAuth = document.querySelector('.auth_btn'); 
+const closeBtn = document.querySelector('.auth-modal-close');
 
-console.log(btnAuth);
+btnAuth.addEventListener('click', (e) => {
+    e.preventDefault();
+    modalOpen.classList.add('active');
+    modalBox.classList.add('active');
+});
 
-// btnAuth.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     modalOpen.classList.add('active');
-//     modalBox.classList.add('active');
-// });
+closeBtn.addEventListener('click', () => {
+    modalOpen.classList.remove('active');
+    modalBox.classList.remove('active');
+});
 
-// closeBtn.addEventListener('click',() => {
-//     modalOpen.classList.remove('active');
-//     modalBox.classList.remove('active');
-// });
+document.addEventListener('click', (e) => {
+    if(e.target === modalOpen) {
+        modalOpen.classList.remove('active');
+        modalBox.classList.remove('active');
+    }
+});
 
-// document.addEventListener('click', (e) => {
-//     if(e.target === modalOpen) {
-//         modalOpen.classList.remove('active');
-//         modalBox.classList.remove('active');
-//     }
-// });
-// Authorisation script
+// Authorication script
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAOAAbZA3RU8RhMKF_OMDcBQlQNDXUrEUg",
-//   authDomain: "filmoteka-team-js-project.firebaseapp.com",
-//   projectId: "filmoteka-team-js-project",
-//   storageBucket: "filmoteka-team-js-project.appspot.com",
-//   messagingSenderId: "48080784167",
-//   appId: "1:48080784167:web:db61b804267a6c3c8e7702",
-//   measurementId: "G-LG80QD4W41"
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyAOAAbZA3RU8RhMKF_OMDcBQlQNDXUrEUg",
+  authDomain: "filmoteka-team-js-project.firebaseapp.com",
+  databaseURL: "https://filmoteka-team-js-project-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "filmoteka-team-js-project",
+  storageBucket: "filmoteka-team-js-project.appspot.com",
+  messagingSenderId: "48080784167",
+  appId: "1:48080784167:web:4008dc2d03d12f778e7702",
+  measurementId: "G-T8P7HD6DV7"
+};
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
 
-// const app = initializeApp(firebaseConfig);
-// const uiConfig = {
-//         signInSuccessUrl: '<url-to-redirect-to-on-success>', // Waiting for complete modal link
-//         signInOptions: [
-//           // Leave the lines as is for the providers you want to offer your users.
-//           firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-//           firebase.auth.GithubAuthProvider.PROVIDER_ID,
-//           firebase.auth.EmailAuthProvider.PROVIDER_ID,
-//           firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-//           firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
-//         ],
-// };
+const createAccount = async () => {
+    const loginEmail = document.getElementById('email').value;
+    const loginPassword = document.getElementById('password').value;
+    console.log(loginEmail);
+    console.log(loginPassword);
+    
+    try {
+        const userCredential = await createUserWithEmailAndPassword(auth, loginEmail, loginPassword);
+        console.log(userCredential.user);
+    } catch (error) {
+        console.log(error.name);
+        console.log(error.message);
+    }
+};
 
-// // Initialize the FirebaseUI Widget using Firebase.
-// const ui = new firebaseui.auth.AuthUI(firebase.auth());
-// // The start method will wait until the DOM is loaded.
-// ui.start('#firebaseui-auth-container', uiConfig);
+const registerBtn = document.querySelector('#registerBtn');
+registerBtn.addEventListener('click', createAccount);
+
+const logInUser = async () => {
+    const loginEmail = document.getElementById('email').value;
+    const loginPassword = document.getElementById('password').value;
+    console.log(loginEmail);
+    console.log(loginPassword);
+    
+    try {
+        const userCredential = await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
+        console.log(userCredential.user);
+        hideLogInError();
+    } catch (error) {
+        console.log(error.name);
+        console.log(error.message);
+        showEmailError(error);
+        showPasswordError(error);
+    }
+};
+
+const logInBtn = document.querySelector('#logInBtn');
+logInBtn.addEventListener('click', logInUser);
+
+//LogIn mistake
+const emailWarningMessage = document.querySelector('.warning-message__email')
+const passwordWarningMessage = document.querySelector('.warning-message__password');
+const hideLogInError = () => {
+    emailWarningMessage.style.display = 'none';
+    emailWarningMessage.innerHTML = '';
+    passwordWarningMessage.style.display = 'none';
+    passwordWarningMessage.innerHTML = '';
+};
+const showEmailError = (error) => {
+    emailWarningMessage.style.display = 'inline-block';
+    if (error.code === AuthErrorCodes.CREDENTIAL_ALREADY_IN_USE) {
+        emailWarningMessage.innerHTML = 'That email already in use.';
+    } else {
+        emailWarningMessage.innerHTML = `Error: ${error.message}`;
+    };
+};
+
+const showPasswordError = (error) => {
+    passwordWarningMessage.style.display = 'inline-block';
+    if (error.code === AuthErrorCodes.INVALID_PASSWORD) {
+        passwordWarningMessage.innerHTML = 'Wrong password. Try again, please.';
+    } else {
+        passwordWarningMessage.innerHTML = `Error: ${error.message}`;
+    };
+}
+
+
+const showUserState = (user) => {
+    let message = '11:04 на видео, если понадобится'
+};
+
+hideLogInError();
+
+const monitorAuthState = async () => {
+    onAuthStateChanged(auth, user => {
+        if (user) {
+            console.log(user);
+        } else {
+
+        };
+    })
+};
+monitorAuthState();
