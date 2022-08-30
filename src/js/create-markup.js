842f64b31ed5a9315f5f011a1334b4b8236e3382
Yurii-Naumchuk <98982620+Yurii-Naumchuk@users.noreply.github.com>
2022-08-30 22:37:22 +0300

    fixed bags

--- src/js/create-markup.js
+++ src/js/create-markup.js
@@ -4,6 +4,9 @@ import { addCurrrentMoviesToLocalStorage } from "./local-storage"
 
 const IMG_URL = "https://image.tmdb.org/t/p/w500"
 
+
+
+
 ///////////// --функція для створення карток популярних фільмів на головну сторінку----///////
 function renderFilmList(films) {
     const markup = films
@@ -14,7 +17,9 @@ function renderFilmList(films) {
         data-modal-open>
         <img class="card-poster"
         data-id="${film.id}" 
-        src="${IMG_URL}${film.poster_path}" 
+        src="${film.poster_path === null ? './no_image.jpg'
+              : `${IMG_URL}${film.poster_path}`
+          }"
         alt="${film.original_name}" loading="lazy">
         <div class="card-wrap">
         <h2 class="card-title" data-id="${film.id}">${film.original_title.toUpperCase() || film.title.toUpperCase() || film.title.toUpperCase()}</h2>
@@ -58,9 +63,7 @@ function transformId ([...arr]) {
     const genres = JSON.parse(g);
     let genreName;
     const array = [...arr]
-    // console.log(genres)
     for (let i = 0; i < genres.length; i++) {
-        // console.log(genres[i]);
         for (let x = 0; x < array.length; x++) {
             if (array[x] === genres[i].id) {
                 genreName = genres[i].name;
@@ -68,8 +71,7 @@ function transformId ([...arr]) {
             };
         };
     };
-    console.log(array)
-    if (array.length > 2) {
+     if (array.length > 2) {
         return `${array[0]}, ${array[1]}, Other`
     }
     return `${array[0]}, ${array[1]}`
