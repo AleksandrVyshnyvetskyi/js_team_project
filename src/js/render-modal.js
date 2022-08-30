85633e50b11e042e8137db18a2875ae7ba005559
Illya-Bilyk <bilikillya.i@gmail.com>
2022-08-31 01:38:03 +0300

    2

--- src/js/render-modal.js
+++ src/js/render-modal.js
@@ -37,7 +37,7 @@ function filmMarkup(film) {
 
   const IMG_URL = 'https://image.tmdb.org/t/p/w500';
   return `
-  <div class="container">
+  <div class="modal-wrap">
       
       <div class="modal-container">
         <div class="modal-left">
