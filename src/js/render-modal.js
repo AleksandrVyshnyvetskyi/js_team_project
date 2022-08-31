96474648aab4871b581031daf4077dee30023f08
Illya-Bilyk <bilikillya.i@gmail.com>
2022-08-31 17:51:53 +0300

    1

--- src/js/render-modal.js
+++ src/js/render-modal.js
@@ -80,10 +80,10 @@ function filmMarkup(film) {
           <p class="about-desc">${aboutOverview}</p>
       </div>
       <div class="modal-button">
-        <button class="btn watched-btn" type="button">ADD TO WATCHED</button>
-        <button class="btn queue-btn" type="button">ADD TO QUEUE</button>
-         <button class="btn watched-btn is-hidden" type="button">REMOVE FROM WATCHED</button>
-        <button class="btn queue-btn is-hidden" type="button">REMOVE FROM QUEUE</button>
+        <button class="btn watched-btn" type="button" data-add>ADD TO WATCHED</button>
+        <button class="btn queue-btn" type="button" data-add>ADD TO QUEUE</button>
+         <button class="btn watched-btn is-hidden" type="button" data-remove>REMOVE FROM WATCHED</button>
+        <button class="btn queue-btn is-hidden" type="button" data-remove>REMOVE FROM QUEUE</button>
       </div>
           </div>
         </div>
