eed34d1cf9bc679a137d4f2a32f47b81bcf23602
OLEKSANDR VYSHNYVETSKYI <95502319+AleksandrVyshnyvetskyi@users.noreply.github.com>
2022-08-31 23:24:57 +0300

    Update create-markup.js

--- src/js/create-markup.js
+++ src/js/create-markup.js
@@ -9,27 +9,48 @@ const IMG_URL = "https://image.tmdb.org/t/p/w500"
 
 
 ///////////// --функція для створення карток популярних фільмів на головну сторінку----///////
-function renderFilmList(films) {
-    const markup = films
-        .map((film) => {
-//         console.log(film)
-            return `
-            <li class="main-container--card"
+function renderMoviesCard(films) {
+    const filmCards = films
+      .map((film) => {
+        if (!film.poster_path) {
+          return `
+        <li
+          class="main-container--card"
+          data-modal-open>
+            <img class="card-poster"
+            data-id="${film.id}"
+            src="https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg" >
+          <div
+            class="card-wrap">
+            <h2
+              class="card-title"
+              data-id="${film.id}">
+              ${film.original_title.toUpperCase() || film.title.toUpperCase() || film.title.toUpperCase()}
+            </h2>
+            <p
+              class="card-info">
+              ${transformId(film.genre_ids)} | ${(film.release_date || film.first_air_date).slice(0, 4)}
+                <span class="card-rating">
+                  ${film.vote_average.toFixed(1)}
+                </span></p>
+          </div>
+        </li>`;
+        };
+        return `
+        <li class="main-container--card"
         data-modal-open>
-        <img class="card-poster"
-        data-id="${film.id}" 
-        src="${film.poster_path === null ? './no_image.jpg'
-              : `${IMG_URL}${film.poster_path}`
-          }"
+          <img class="card-poster"
+        data-id="${film.id}"
+        src="${IMG_URL}${film.poster_path}"
         alt="${film.original_name}" loading="lazy">
         <div class="card-wrap">
         <h2 class="card-title" data-id="${film.id}">${film.original_title.toUpperCase() || film.title.toUpperCase() || film.title.toUpperCase()}</h2>
-        <p class="card-info"> ${transformId(film.genre_ids)} | ${(film.release_date || film.first_air_date).slice(0,4)} </p>
+        <p class="card-info"> ${transformId(film.genre_ids)} | ${(film.release_date || film.first_air_date).slice(0,4)} <span class="card-rating"> ${film.vote_average.toFixed(1)}</span></p>
          </div>
     </li>`;
         }).join("");
-refs.containerBox.innerHTML = markup;
-}
+    refs.moviesContainer.insertAdjacentHTML('beforeend', filmCards);
+};
 
 export {renderFilmList};
 
