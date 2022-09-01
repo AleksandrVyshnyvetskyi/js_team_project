c4416224283d92a190e9026d3777c635d5e50a37
OLEKSANDR VYSHNYVETSKYI <95502319+AleksandrVyshnyvetskyi@users.noreply.github.com>
2022-09-01 20:25:13 +0300

    Update local-storage.js

--- src/js/local-storage.js
+++ src/js/local-storage.js
@@ -1,24 +1,10 @@
 import refs from "./refs";
 
-export default class LocalStorage { 
-    constructor(){ 
-        this.watched = [];
-        this.queue = [];
-    }
-
-    addToLSwathced(key) { 
-      localStorage.setItem(key, JSON.stringify(this.watched));
-    }
-
-
-   addToLSqueue (key) { 
-      localStorage.setItem(key, JSON.stringify(this.queue));
-    }
-    
-};
-
-export function addCurrrentMoviesToLocalStorage(array) { 
+function addCurrrentMoviesToLocalStorage(array) { 
 localStorage.setItem("MOVIE_LIST", JSON.stringify(array))
 };
+
+
+export { addCurrrentMoviesToLocalStorage };
 
 
