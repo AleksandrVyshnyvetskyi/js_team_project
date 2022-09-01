2699c0e9527860646065d5955094a1aa1e2c8c58
OLEKSANDR VYSHNYVETSKYI <95502319+AleksandrVyshnyvetskyi@users.noreply.github.com>
2022-09-01 20:53:18 +0300

    Update modal-buttons.js

--- src/js/modal-buttons.js
+++ src/js/modal-buttons.js
@@ -1,87 +1,34 @@
 import refs from "./refs";
-import LocalStorage from './local-storage'
 
 const MOVIE_LIST_KEY = "MOVIE_LIST";
 const WATCHED_LIST_KEY = "WATCHED_LIST"; 
 const QUEUE_LIST_KEY = "QUEUE_LIST";
 
+let watchedArray = [];
+let queueArray = [];
+localStorage.setItem(WATCHED_LIST_KEY, JSON.stringify(watchedArray));
+localStorage.setItem(QUEUE_LIST_KEY, JSON.stringify(queueArray));
 
-
-
-const localStor = new LocalStorage;
-let watchedArray = localStor.watched;
-let queueArray = localStor.queue;
-
-export function addWathcedListener(filmId) { 
-   
+export function addListener(filmId) { 
     const btnAddWatched = document.querySelector("[data-addwatched]");
     const btnRemoveWatched = document.querySelector("[data-removewatched]");
 
-    const isList = localStorage.getItem(WATCHED_LIST_KEY);
-    const isMovieinWatchedList = movieOfWatchedList(filmId);
-    
-   
-    if (!isList) {
-        console.log("нету");
-        btnAddWatched.addEventListener("click", onAddWathedClick);
-    }
-    else if (isMovieinWatchedList) {
+    const btnAddQueue = document.querySelector("[data-addqueue]");
+    const btnRemoveQueue = document.querySelector("[data-removequeue]");
+
+    const isMovieinWatchedList = movietoWatchedList(filmId);
+    const isMovieinQueueList = movietoQueueList(filmId);
+
+    if (isMovieinWatchedList) {
         btnAddWatched.classList.add("is-hidden");
         btnRemoveWatched.classList.remove("is-hidden");
         btnRemoveWatched.addEventListener("click", onRemoveWathedClick);
     }
-    else { btnAddWatched.addEventListener("click", onAddWathedClick); }
-
-
-    
-
-    
-    function onAddWathedClick(e) { 
-        e.preventDefault();
-        const film = getObjectbyId(filmId);
-        if (!isMovieinWatchedList || !isList) { 
-             localStor.addToLSwathced(WATCHED_LIST_KEY);
-            watchedArray.push(film); 
-            btnAddWatched.classList.add("is-hidden");
-            btnRemoveWatched.classList.remove("is-hidden");   
-            btnRemoveWatched.addEventListener("click", onRemoveWathedClick);
-           
-        }
-         localStor.addToLSwathced(WATCHED_LIST_KEY);
-       
-        
-    };
-    
-    function onRemoveWathedClick(e) {
-
-        btnAddWatched.classList.remove("is-hidden");
-        btnRemoveWatched.classList.add("is-hidden");
-        btnAddWatched.addEventListener("click", onAddWathedClick);
-       
-        const film = getObjectbyId(filmId);
-        const watched_list = localStorage.getItem(WATCHED_LIST_KEY);
-        const watched_list_array = JSON.parse(watched_list);
-        const indexFilm = watched_list_array.findIndex(fil => fil.id === film.id);
-        watchedArray.splice(indexFilm, 1);
-        localStorage.setItem(WATCHED_LIST_KEY, JSON.stringify(watchedArray));
+    else { 
+           btnAddWatched.addEventListener("click", onAddWathedClick);
     }
 
-}
-
-
-export function addQueueListener(filmId) {
-    
-    const btnAddQueue = document.querySelector("[data-addqueue]");
-    const btnRemoveQueue = document.querySelector("[data-removequeue]");
-    
-    const isMovieinQueueList = movieOfQueueList(filmId);
-    const isList = localStorage.getItem(QUEUE_LIST_KEY);
-
-
-     if (!isList) {
-        btnAddQueue.addEventListener("click", onAddQueueClick);
-     }
-     else if (isMovieinQueueList) {
+     if (isMovieinQueueList) {
         btnAddQueue.classList.add("is-hidden");
         btnRemoveQueue.classList.remove("is-hidden");
         btnRemoveQueue.addEventListener("click", onRemoveQueueClick);
@@ -89,19 +36,19 @@ export function addQueueListener(filmId) {
     else { 
            btnAddQueue.addEventListener("click", onAddQueueClick);
     }
+    function onAddQueueClick(e) { 
+        console.log("hi");
 
-     function onAddQueueClick(e) { 
         e.preventDefault();
-         const film = getObjectbyId(filmId);
-         
-         if (!isMovieinQueueList || isList) { 
-            localStor.addToLSqueue(QUEUE_LIST_KEY);
+        const film = getObjectbyId(filmId);
+        if (!isMovieinQueueList) { 
             queueArray.push(film); 
             btnAddQueue.classList.add("is-hidden");
             btnRemoveQueue.classList.remove("is-hidden");   
-            btnRemoveQueue.addEventListener("click", onRemoveQueueClick);   
-        }   
-         localStor.addToLSqueue(QUEUE_LIST_KEY);
+            btnRemoveQueue.addEventListener("click", onRemoveQueueClick);
+            
+        }
+        localStorage.setItem(QUEUE_LIST_KEY, JSON.stringify(queueArray));
 
     }
 
@@ -120,42 +67,59 @@ export function addQueueListener(filmId) {
     }
 
 
- };
+    
+    function onAddWathedClick(e) { 
+        e.preventDefault();
+        const film = getObjectbyId(filmId);
+        if (!isMovieinWatchedList) { 
+            watchedArray.push(film); 
+            btnAddWatched.classList.add("is-hidden");
+            btnRemoveWatched.classList.remove("is-hidden");   
+            btnRemoveWatched.addEventListener("click", onRemoveWathedClick);
+            
+        }
+        localStorage.setItem(WATCHED_LIST_KEY, JSON.stringify(watchedArray));
+        
+    };
+    
+    function onRemoveWathedClick(e) {
 
-function movieOfWatchedList(filmid) {
-    const watched_list = localStorage.getItem(WATCHED_LIST_KEY);
-    if (!watched_list) {
-        return null;
+        btnAddWatched.classList.remove("is-hidden");
+        btnRemoveWatched.classList.add("is-hidden");
+        btnAddWatched.addEventListener("click", onAddWathedClick);
+       
+        const film = getObjectbyId(filmId);
+        const watched_list = localStorage.getItem(WATCHED_LIST_KEY);
+        const watched_list_array = JSON.parse(watched_list);
+        const indexFilm = watched_list_array.findIndex(fil => fil.id === film.id);
+        watchedArray.splice(indexFilm, 1);
+        localStorage.setItem(WATCHED_LIST_KEY, JSON.stringify(watchedArray));
     }
-    else {
-    const watched_list_array = JSON.parse(watched_list);
-   return watched_list_array.find(f => f.id === filmid);
-     }
- 
+
+
+}
+// movietoWatchedList  --- movieObjectOfWatchedList
+function movietoWatchedList (filmid) {
+// проеверяю есть ли фильи в локал сторадже 
+  const watched_list = localStorage.getItem(WATCHED_LIST_KEY);
+  const watched_list_array = JSON.parse(watched_list);
+  return watched_list_array.find(f => f.id === filmid);
   
 }; 
 
-function movieOfQueueList(filmid) {
-    const queue_list = localStorage.getItem(QUEUE_LIST_KEY);
-    if (!queue_list) {
-        return null;
-    } else { 
-         const queue_list_array = JSON.parse(queue_list);
+// movietoWatchedList  --- movieObjectOfQueueList
+  
+function movietoQueueList(filmid) {
+ const queue_list = localStorage.getItem(QUEUE_LIST_KEY);
+  const queue_list_array = JSON.parse(queue_list);
   return queue_list_array.find(f => f.id === filmid);
-    }
- 
-};
+ }
 
-function getObjectbyId(filmId) { 
+
+ function getObjectbyId(filmId) { 
      const movie_list = localStorage.getItem(MOVIE_LIST_KEY)
     const array = JSON.parse(movie_list)
     let objectMovie = array.find(films => films.id === filmId);
     return objectMovie;
-};
- 
-
-
- 
-
-
+ }
 
